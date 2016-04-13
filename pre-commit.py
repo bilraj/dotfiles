b4b4156d1abe7743cb97ded38d621a89111d67d8
Bilal Raja <bilraj@umich.edu>
2016-04-13 12:37:36 -0400

    More stuff added

--- pre-commit.py
+++ pre-commit.py
@@ -1,45 +1,35 @@
 import sys
 import subprocess 
+import glob
 
-class Compiler:
+try:
+	p = subprocess.check_call([r"/usr/bin/g++", "-Wall", "-o", "test", file_to_compile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-	function_defs = ""
+except subprocess.CalledProcessError as grepx:
+	print("Compilation error in " + file_to_compile)
+	p.terminate()
 
-	def __init__(self, file_to_compile):
-		try:
-			p = subprocess.check_call([r"/usr/bin/g++", "-Wall", "-o", "test", file_to_compile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+for file in glob.glob("test*.cpp"):
+	s = file.split(".")
+	o = s[0]
+	try:
+		p = subprocess.Popen([r"/usr/bin/g++", "-Wall", "-o", o, file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	except subprocess.CalledProcessError as grepx:
+		print("Compilation error in " + file)
+		p.terminate()
 
-		except subprocess.CalledProcessError as grepx:
-			print("Compilation error in " + file_to_compile)
-			p.terminate()
-	
-	def compile_tests(self, tests):
-		for file in tests:
-			s = file.split(".")
-			o = s[0]
-		try:
-			p = subprocess.Popen([r"/usr/bin/g++", "-Wall", "-o", o, file + function_defs], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		except subprocess.CalledProcessError as grepx:
-			print("Compilation error in " + file)
-	def def_file(self, name):
-		self.function_defs = name
-		print("Name is: " + self.function_defs)
-
-	def run_tests(self, tests):
-		count = 0
-		success = 0
-		for file in tests:
-			try:
-				s = file.split(".")
-				ex = s[0]
-				p = subprocess.Popen(["./" + ex], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-				p.communicate()
-				success += 1
-				count += 1
-				print("Success")
-				
-			except subprocess.CalledProcessError as grepx:
-				print("Test failed")
-				count += 1
+#for file in tests:
+#	try:
+#		s = file.split(".")
+#		ex = s[0]
+#		p = subprocess.Popen(["./" + ex], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#		p.communicate()
+#		success += 1
+#		count += 1
+#		print("Test suceeded")
+#		
+#	except subprocess.CalledProcessError as grepx:
+#		print("Test failed")
+#		count += 1
 				
 
